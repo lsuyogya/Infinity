5ad337101d8abb1a76bc43495ab8828157b90847
Suyogya Luitel <lsuyogya@gmail.com>
2024-10-17 17:53:06 +0545

    progress sync

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,18 @@
-const themeBtn = document.getElementById("colorTheme");
-themeBtn.addEventListener("click", () => {
-  document.querySelector("body").toggleAttribute("dark");
+const themeBtn = document.getElementById('colorTheme');
+themeBtn.addEventListener('click', () => {
+  document.querySelector('body').toggleAttribute('dark');
+});
+const fontPlusBtn = document.getElementById('fontPlus');
+fontPlusBtn.addEventListener('click', () => {
+  const bodyStyles = getComputedStyle(document.body);
+  const fontFactor =
+    parseFloat(bodyStyles.getPropertyValue('--fontFactor').trim()) ?? 1;
+  document.body.style.setProperty('--fontFactor', fontFactor + 0.1);
+});
+const fontMinusBtn = document.getElementById('fontMinus');
+fontMinusBtn.addEventListener('click', () => {
+  const bodyStyles = getComputedStyle(document.body);
+  const fontFactor =
+    parseFloat(bodyStyles.getPropertyValue('--fontFactor').trim()) ?? 1;
+  document.body.style.setProperty('--fontFactor', fontFactor - 0.1);
 });
