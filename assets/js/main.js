42135f816958aa8a70caefe618e9042761e5a60e
Suyogya Luitel <lsuyogya@gmail.com>
2024-10-18 11:30:40 +0545

    initial design finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,20 @@ fontMinusBtn.addEventListener('click', () => {
     parseFloat(bodyStyles.getPropertyValue('--fontFactor').trim()) ?? 1;
   document.body.style.setProperty('--fontFactor', fontFactor - 0.1);
 });
+
+// ACCORDION
+const btnArray = document.querySelectorAll('.accordion-button');
+btnArray.forEach((btn) =>
+  btn.addEventListener('click', () => {
+    const target = document.querySelector(btn.getAttribute('data-bs-target'));
+    if (target) {
+      target.classList.toggle('show');
+      const currentAttr = btn.getAttribute('aria-expanded') ?? 'false';
+      if (currentAttr == 'true') {
+        btn.setAttribute('aria-expanded', 'false');
+      } else {
+        btn.setAttribute('aria-expanded', 'true');
+      }
+    }
+  })
+);
